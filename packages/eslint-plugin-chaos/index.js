9bfb486ae6026f6079f62541fef7b3932a633112
DESKTOP-OOLRT35\Administrator <1193784290@qq.com>
2025-04-09 09:43:10 +0800

    feat: lint

--- packages/eslint-plugin-chaos/index.js
+++ packages/eslint-plugin-chaos/index.js
@@ -1,18 +1,17 @@
-const path = require("path");
-const requireAll = require("require-all");
+const path = require('path');
+const requireAll = require('require-all');
 
 exports.rules = requireAll({
-  dirname: path.resolve(__dirname, "rules"),
+  dirname: path.resolve(__dirname, 'rules'),
 });
 
 exports.configs = requireAll({
-  dirname: path.resolve(__dirname, "configs"),
+  dirname: path.resolve(__dirname, 'configs'),
 });
 
 exports.processors = {
-  ".json": {
+  '.json': {
     preprocess(text) {
-      // As JS file
       return [`module.exports = ${text}`];
     },
   },
